a3d71aaaae85d1ac6769172c7bf96b90cace85f8
juandgranada <juan.granada59204@ucaldas.edu.co>
2025-10-03 16:28:12 -0500

    creando nuevas migraciones y adicionando el usuario administrador por defecto

--- database/migrations/2025_10_03_153337_create_usuarios_table.php
+++ database/migrations/2025_10_03_153337_create_usuarios_table.php
@@ -10,15 +10,27 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('persona_id');
+            $table->unsignedBigInteger('persona_id')->nullable();
             $table->string('username')->unique();
             $table->string('password');
             $table->enum('rol', ['ADMINISTRADOR', 'MEDICO', 'PACIENTE']);
             $table->timestamps();
 
             $table->foreign('persona_id')->references('id')->on('personas')->onDelete('cascade');
+
         });
+
+        DB::table('usuarios')->insert([
+            'persona_id' => null, // el admin no está vinculado a una persona
+            'username'   => 'administrador',
+            'password'   => Hash::make('Admin123$$'),
+            'rol'        => 'ADMINISTRADOR',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
+
+    
 
     public function down(): void
     {
