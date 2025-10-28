a72641edcc88ddd81b2490664b3e699fb7300f42
juandgranada <juan.granada59204@ucaldas.edu.co>
2025-10-28 14:08:31 -0500

    implementacion de panel administrativo con adminlte

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Panel de Administración')
+
+@section('content_header')
+    <h1>Bienvenido al Panel de Administración</h1>
+@stop
+
+@section('content')
+    <p>Este es tu nuevo panel con AdminLTE 🎉</p>
+@stop
--- resources/views/medicos/index.blade.php
+++ resources/views/medicos/index.blade.php
@@ -1,4 +1,6 @@
-@extends('layouts.app')
+<!-- @extends('layouts.app') -->
+ @extends('dashboard')
+
 
 @section('content')
 <div class="container">
